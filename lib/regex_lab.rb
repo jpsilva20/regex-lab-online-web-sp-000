a857b0aa9deb9fdd3cf0f779760701cf12596966
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-16 16:46:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,6 +25,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if 
+  if !phone.scan(/\d{10}|[(]\d{3}[)]\d{3}-\d{4}\b|\d{3}\s\d{3}\s\d{4}|[(]\d{3}[)]\d{7}/).empty?
+    true 
+  else 
+    false 
 
 end
